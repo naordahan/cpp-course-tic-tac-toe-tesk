0c0fb977f6f315270f93333bc508137182359d6a
sholmiw <prin1238@gmail.com>
2018-06-03 16:09:20 +0100

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -92,7 +92,7 @@ ostream&operator <<(ostream& os ,const Board& theboard){
         for (int j = 0; j <theboard.bsize ; ++j) {
             os<<theboard.boardmatrix[i][j];
         }
-        os<<endl;
+        os<<'\n';
     }
     return os;
 }
@@ -122,44 +122,22 @@ istream& operator >>(istream& cin , Board& theboard){
 //so far so good
 //TODO: make it work
 //TODO:fix this one: not working 
-// void Dcircle(RGB* imge,int column,int row,int cellsize,int dimy){
-//     //func tha draw circle
-//     int r = cellsize/2 , x_centre = (xto-xfrom)/2 , y_centre = (Yto-Yfrom)/2;
-//     int x = r, y = 0;
-//     imge[(x + x_centre)+(y + y_centre)]={0,0,255};
-//     if (r > 0)
-//     {
-//       //  imge[(x + x_centre][-y + y_centre]={0,0,255}; = origenl
-//         imge[(x + x_centre)+(-y + y_centre)]={0,0,255};
-//         imge[(y + x_centre)+(x + y_centre)]={0,0,255};
-//         imge[(-y + x_centre)+(x + y_centre)]={0,0,255};
-//     int P = 1 - r;
-//     while (x > y)
-//     {
-//         y++;
-//         if (P <= 0)
-//             P = P + 2*y + 1;
-//         else
-//         {
-//             x--;
-//             P = P + 2*y - 2*x + 1;
-//         }
-//         if (x < y)
-//             break;
-//         imge[(x + x_centre)+(y + y_centre)]={0,0,255};
-//         imge[(-x + x_centre)+(y + y_centre)]={0,0,255};
-//         imge[(x + x_centre)+(-y + y_centre)]={0,0,255};
-//         imge[(-x + x_centre)+(-y + y_centre)]={0,0,255};
-//         if (x != y)
-//         {
-//             imge[(y + x_centre)+(x + y_centre)]={0,0,255};
-//             imge[(-y + x_centre)+(x + y_centre)]={0,0,255};
-//             imge[(y + x_centre)+(-x + y_centre)]={0,0,255};
-//             imge[(-y + x_centre)+(-x + y_centre)]={0,0,255};
-//         }
-// }
-//     }
-// }
+void Dcircle(RGB* imge,int Xfrom,int Xto,int Yfrom,int Yto,int dimy){
+    int R = ((Xto - Xfrom) / 2) -(int)((Xto-5 - Xfrom+5)%10);  //redius - need to fix
+				int Ox = (Xto - Xfrom) / 2 ;      // xmid
+				int Oy = (Yto - Yfrom) / 2 ;      //ymid
+    //  a nicer circale with the help of yossi
+                for (int y=0; y<2*R; y++){
+                    for (int x=0; x<2*R; x++){
+                        if ( floor(sqrt( (x-Ox)*(x-Ox) + (y-Oy)*(y-Oy) ) ) == R-10 ) {
+                            imge[dimy * (y + Yfrom) + Xfrom + x]={0,0,255};
+                            imge[dimy * (y + Yfrom) + Xfrom + x+1]={0,0,255};
+                            imge[dimy * (y + Yfrom) + Xfrom + x+2]={0,0,255};
+                            imge[dimy * (y + Yfrom) + Xfrom + x-1]={0,0,255};
+                        }
+                    }
+                }              
+}
 void Dex(RGB* imge,int Xfrom,int Xto,int Yfrom,int Yto,int dimy){
     //func tha draw x
     for (int r = 10; r < Yto - Yfrom - 10 ; ++r) {
@@ -180,7 +158,6 @@ void Dex(RGB* imge,int Xfrom,int Xto,int Yfrom,int Yto,int dimy){
 string Board::draw(int nPx){
     const int dimx=nPx;
     const int dimy=nPx;
-    //cout <<"dimx="<<dimx<<'\n';
     string fileName="myboard.ppm";
     ifstream file(fileName);
     bool myfileExist=false;
@@ -200,21 +177,17 @@ string Board::draw(int nPx){
     }
     //create new file
     ofstream imgFile(fileName);
-    //cout<< "letsgo";
-    //cout<<"my Board:"<<fileName<<'\n';
-    imgFile<<"P6"<<'\n'<< dimx << " " << dimy << endl << 255 <<endl;
+    imgFile<<"P6"<<'\n'<< dimx << " " << dimy << '\n' << 255 <<'\n';
     // start building the pic:   
     // create mat for pixel:   
     RGB imge[dimx*dimy];
-    //cout <<"imge.length()=" << dimx*dimy-1 ;
     //background color
     for(int j=0; j<dimy;j++){  // row
        for(int i=0; i<dimx;i++){ // column               
         //    imge[(dimx*j)+i].red = (i % 256);
         //    imge[(dimx*j)+i].green = (j % 256);
         //    imge[(dimx*j)+i].blue = ( ((i*i)+(j*j)) % 256);
-           imge[(dimx*j)+i]={240,240,24};
-           
+           imge[(dimx*j)+i]={240,240,24};           
          }
     } 
     int cellsize = dimx/bsize; //size for each coordinate
@@ -223,18 +196,14 @@ string Board::draw(int nPx){
 	for (int i = 0; i < bsize; ++i) {
 		int Xfrom = i * (cellsize);
 		for (int r = 3; r < dimy - 3 ; ++r) {
-            imge[dimy * (r) + Xfrom]={0,0,0};
-			
+            imge[dimy * (r) + Xfrom]={0,0,0};			
 		}
 	}
 	//Drawing the rows
 	for (int j = 0; j < bsize; ++j) {
-
 		int Yfrom = j * (cellsize) ;
-
 		for (int r = 3; r < dimx - 3 ; ++r) {
             imge[(r) + Yfrom * dimy]={0,0,0};
-
 		}
 	}
 	int loop = 0;
@@ -246,61 +215,40 @@ string Board::draw(int nPx){
 			int Xto = (column + 1) * (cellsize); //dont include the last bit
 			int Yfrom = row * (cellsize) ;
 			int Yto = (row + 1) * (cellsize); //dont include the last bit
-
 			if (boardmatrix[row][column] == 'X') {
                 //Drawing X
-                Dex(imge,Xfrom,Xto,Yfrom,Yto,dimy);  
-			
+                Dex(imge,Xfrom,Xto,Yfrom,Yto,dimy);  			
 			}
 			else if(boardmatrix[row][column] == 'O'){
 				//Drawing O
-                // Dcircle(imge,column,row,cellsize,dimy);
+                
                 //from https://github.com/GeekCSA/Exercise-8---Draw/blob/master/Board.cpp
 
-				int stepFromCenter = 0;
-				bool arriveToCenter = false;
-				int halfsize = (Xto - Xfrom) / 2;
+				// int R = ((Xto - Xfrom) / 2) -(int)((Xto-5 - Xfrom+5)%10);  //redius - need to fix
+				// int Ox = (Xto - Xfrom) / 2 ;      // xmid
+				// int Oy = (Yto - Yfrom) / 2 ;      //ymid
 
-				int R = (Xto - Xfrom) / 2;
-				int Ox = (Xto - Xfrom) / 2;
-				int Oy = (Yto - Yfrom) / 2;
-
-				for (int X = 10; X < Yto - Yfrom - 10 ; ++X) {
-					int Y = sqrt(R*R - (X - Ox)*(X - Ox)) + Oy;
-					int attach = 20;
-					//Drawing bottom size of the circle
-                    imge[dimy * (Y + Yfrom - attach) + Xfrom + X]={0,0,255};
-                    imge[dimy * (Y + Yfrom - attach) + Xfrom + X+1]={0,0,255};
-                    imge[dimy * (Y + Yfrom - attach) + Xfrom + X+2]={0,0,255};
-                    imge[dimy * (Y + Yfrom - attach) + Xfrom + X+3]={0,0,255};
-                    imge[dimy * (Y + Yfrom - attach) + Xfrom + X+4]={0,0,255};
-                    //Drawing upper size of the circle ?
-                    imge[dimy * (Yto - Y + attach) + Xfrom + X]={0,0,255};
-                    imge[dimy * (Yto - Y + attach) + Xfrom + X+1]={0,0,255};
-                    imge[dimy * (Yto - Y + attach) + Xfrom + X+2]={0,0,255};
-                    imge[dimy * (Yto - Y + attach) + Xfrom + X+3]={0,0,255};
-                    imge[dimy * (Yto - Y + attach) + Xfrom + X+4]={0,0,255};
-				}
-                //death star :)
-                /*
-                bool fill=false;
-                for(int i=Xfrom;i<Xto;i++){
-                    for(int j=Yfrom;j<Yto;j++){
-                        if(imge[dimy * i +j].blue == 255) {
-                            fill=!fill;
-                        }
-                        if(fill){
-                           imge[dimy * i +j]={0,0,255}; 
-                        }
-
-                    }
-                }
-				*/
-                
+				// for (int X = 10; X < Yto - Yfrom - 10 ; ++X) {
+				// 	int Y = floor(sqrt(R*R - (X - Ox)*(X - Ox)) + Oy);
+				// 	int attach = 20;
+				// 	//Drawing bottom size of the circle
+                //     imge[dimy * (Y + Yfrom - attach) + Xfrom + X]={0,0,255};
+                //     imge[dimy * (Y + Yfrom - attach) + Xfrom + X+1]={0,0,255};
+                //     imge[dimy * (Y + Yfrom - attach) + Xfrom + X+2]={0,0,255};
+                //     imge[dimy * (Y + Yfrom - attach) + Xfrom + X+3]={0,0,255};
+                //     imge[dimy * (Y + Yfrom - attach) + Xfrom + X+4]={0,0,255};
+                //     //Drawing upper size of the circle ?
+                //     imge[dimy * (Yto - Y + attach) + Xfrom + X]={0,0,255};
+                //     imge[dimy * (Yto - Y + attach) + Xfrom + X+1]={0,0,255};
+                //     imge[dimy * (Yto - Y + attach) + Xfrom + X+2]={0,0,255};
+                //     imge[dimy * (Yto - Y + attach) + Xfrom + X+3]={0,0,255};
+                //     imge[dimy * (Yto - Y + attach) + Xfrom + X+4]={0,0,255};
+				// }
+                //  a nicer circle with the help of yossi
+                 Dcircle(imge,Xfrom,Xto,Yfrom,Yto,dimy);    
 			}
 		}
 	}
-
     imgFile.write(reinterpret_cast<char*>(&imge),3*dimx*dimy);
     imgFile.close();
     return fileName;
